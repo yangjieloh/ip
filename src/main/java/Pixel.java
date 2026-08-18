fd3079793ad37d17a0bdfb5e20ed65ff96c53366
Yang Jie <254612143+yangjieloh@users.noreply.github.com>
2026-08-19 02:35:37 +0800

    Chatbot echos commands

--- src/main/java/Pixel.java
+++ src/main/java/Pixel.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Starts the Pixel chatbot application.
  */
@@ -14,7 +16,20 @@ public class Pixel {
         System.out.println("Hello! I'm Pixel.");
         System.out.println("What can I do for you?");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            System.out.println(line);
+
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+
+            System.out.println(command);
+            System.out.println(line);
+        }
     }
 }
