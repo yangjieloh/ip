26e4bc0daa3ee36cbae21f9eebee1e16d5bd2b87
Yang Jie <254612143+yangjieloh@users.noreply.github.com>
2026-08-19 02:21:04 +0800

    Rename chatbot to Pixel

---
+++ src/main/java/Pixel.java
@@ -0,0 +1,13 @@
+/**
+ * Starts the Pixel chatbot application.
+ */
+public class Pixel {
+    public static void main(String[] args) {
+        String banner = " ____  _          _ \n"
+                + "|  _ \\(_)_  _____| |\n"
+                + "| |_) | \\ \\/ / _ \\ |\n"
+                + "|  __/| |>  <  __/ |\n"
+                + "|_|   |_/_/\\_\\___|_|\n";
+        System.out.println(banner);
+    }
+}
