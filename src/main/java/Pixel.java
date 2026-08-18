329889c377ac2391b4d4648263016fa308b43448
Yang Jie <254612143+yangjieloh@users.noreply.github.com>
2026-08-19 02:30:01 +0800

    Chatbot greets the user and exit

--- src/main/java/Pixel.java
+++ src/main/java/Pixel.java
@@ -7,7 +7,14 @@ public class Pixel {
                 + "|  _ \\(_)_  _____| |\n"
                 + "| |_) | \\ \\/ / _ \\ |\n"
                 + "|  __/| |>  <  __/ |\n"
-                + "|_|   |_/_/\\_\\___|_|\n";
+                + "|_|   |_/_/\\_\\___|_|";
+        String line = "____________________________________________________________";
+        System.out.println(line);
         System.out.println(banner);
+        System.out.println("Hello! I'm Pixel.");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 }
