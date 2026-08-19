e212f439ccffcc41a5c8bc229d7de6a564410a79
Yang Jie <254612143+yangjieloh@users.noreply.github.com>
2026-08-19 15:14:37 +0800

    Mark command implemented

--- src/main/java/Pixel.java
+++ src/main/java/Pixel.java
@@ -18,8 +18,10 @@ public class Pixel {
         System.out.println(line);
 
         String[] tasks = new String[100];
+        boolean[] isDone = new boolean[100];
         int taskCount = 0;
         Scanner scanner = new Scanner(System.in);
+
         while (scanner.hasNextLine()) {
             String command = scanner.nextLine();
             System.out.println(line);
@@ -29,8 +31,25 @@ public class Pixel {
                 System.out.println(line);
                 break;
             } else if (command.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    String status = isDone[i] ? "X" : " ";
+                    System.out.println((i + 1) + ".[" + status + "] " + tasks[i]);
+                }
+                System.out.println(line);
+            } else if (command.startsWith("mark ")) {
+                try {
+                    int taskNumber = Integer.parseInt(command.substring(5).trim());
+                    int index = taskNumber - 1;
+                    if (index < 0 || index >= taskCount) {
+                        System.out.println("That task number does not exist.");
+                    } else {
+                        isDone[index] = true;
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println("  [X] " + tasks[index]);
+                    }
+                } catch (NumberFormatException exception) {
+                    System.out.println("Please specify a valid task number after mark.");
                 }
                 System.out.println(line);
             } else {
