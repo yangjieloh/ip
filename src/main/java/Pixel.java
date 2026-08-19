aa699620a007339003e7748bb228450196a21914
Yang Jie <254612143+yangjieloh@users.noreply.github.com>
2026-08-19 14:58:32 +0800

    Chatbot can add items to list and list it

--- src/main/java/Pixel.java
+++ src/main/java/Pixel.java
@@ -17,6 +17,8 @@ public class Pixel {
         System.out.println("What can I do for you?");
         System.out.println(line);
 
+        String[] tasks = new String[100];
+        int taskCount = 0;
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             String command = scanner.nextLine();
@@ -26,10 +28,17 @@ public class Pixel {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(line);
                 break;
+            } else if (command.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(line);
+            } else {
+                tasks[taskCount] = command;
+                taskCount++;
+                System.out.println("added: " + command);
+                System.out.println(line);
             }
-
-            System.out.println(command);
-            System.out.println(line);
         }
     }
 }
